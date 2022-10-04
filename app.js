6f15421bbbe42bada9098932a3aa3894e670aed7
LEEGWONSEOK <leestart1023@gmail.com>
2022-10-04 17:51:58 +0900

    Feat: http, socket.io 불러오기

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
-
 const app = express();
+const server = http.createServer(app);
+
+const socketIO = require('socket.io')
 
 app.use(express.static(path.join(__dirname, 'src')));
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`server is running ${PORT}`));
+server.listen(PORT, () => console.log(`server is running ${PORT}`));
