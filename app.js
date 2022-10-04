f2659767dd3921714131af86973b7694bbe4fee8
LEEGWONSEOK <leestart1023@gmail.com>
2022-10-04 18:01:59 +0900

    Feat : socket.io 초기 세팅 완료

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const http = require('http');
 const path = require('path');
 const app = express();
 const server = http.createServer(app);
-
-const socketIO = require('socket.io')
+const socketIO = require('socket.io');
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, 'src')));
-
 const PORT = process.env.PORT || 8080;
+
+io.on('connection', (socket) => {
+  socket.on('chatting', (data) => {
+    console.log(data);
+    io.emit('chatting', `반갑다 ${data}`);
+  });
+})
 
 server.listen(PORT, () => console.log(`server is running ${PORT}`));
